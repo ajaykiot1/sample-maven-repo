fa614ad97c1795d73b0ac9a72b24ec0e5af3d6ab
ajay.kiot1 <ajay.kiot1@gmail.com>
2022-09-08 09:32:32 +0000

    sample project

---
+++ src/main/java/com/inn/restro/sample/maven/SampleMavenApplication.java
@@ -0,0 +1,13 @@
+package com.inn.restro.sample.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/restro/sample/maven/SampleMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.restro.sample.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
